ac32ce94cd2ebe92e870c03601203d84a3184ea8
Felipe Castilhos <felipecastilhos@gmail.com>
2019-02-14 00:22:45 -0200

    Spawn coin

---
+++ Assets/Scripts/CoinSpawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinSpawn : MonoBehaviour {
+    public GameObject coin;
+    public static CoinSpawn instance;
+    public int spawnChance = 0;
+
+    private void Awake() {
+        if (instance == null) instance = this;
+    }
+
+    public void Spawn(Vector3 pos) {
+        spawnChance = Random.Range(0, 3);
+        if(spawnChance == 1) {
+            Debug.Log("Spawn chance: " + spawnChance);
+            Instantiate(coin, pos, Quaternion.identity);
+        }
+    }
+}
--- Assets/Scripts/Destructable.cs
+++ Assets/Scripts/Destructable.cs
@@ -6,13 +6,12 @@ public class Destructable : MonoBehaviour
 {
     [SerializeField] Animator destructionAnimator;
 
-
     IEnumerator OnTriggerEnter2D(Collider2D collision) {
-
         if (collision.gameObject.CompareTag("heroAttack")) {
             Debug.Log("Breaking pot");
             destructionAnimator.Play("Breaking");
-            yield return new WaitForSeconds(1);
+            CoinSpawn.instance.Spawn(transform.position);
+            yield return new WaitForSeconds(0.5f);
             Destroy(gameObject);
     
         }
