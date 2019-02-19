af1a336790bf8076941c099e69db889775de41f6
Felipe Castilhos <felipecastilhos@gmail.com>
2019-02-19 20:59:32 -0300

    Create destructable object base class

--- Assets/Scripts/Destructable.cs
+++ Assets/Scripts/Destructable.cs
@@ -2,19 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Destructable : MonoBehaviour
+public class Destructable : DestructableBase
 {
     [SerializeField] Animator destructionAnimator;
 
     IEnumerator OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("heroAttack")) {
-            Debug.Log("Breaking pot");
-            destructionAnimator.Play("Breaking");
-            CoinSpawn.instance.Spawn(transform.position);
-            yield return new WaitForSeconds(0.5f);
-            Destroy(gameObject);
-    
+            AddDamage(3);
         }
+        yield return 0;
+    }
 
+    public override IEnumerator DoDestroy() {
+        Debug.Log("Breaking pot");
+        destructionAnimator.Play("Breaking");
+        CoinSpawn.instance.Spawn(transform.position);
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/DestructableBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DestructableBase : MonoBehaviour
+{
+    public int resistencyPoints;
+
+    public void AddDamage(int damage) {
+        resistencyPoints -= damage;
+
+        if(resistencyPoints <= 0) {
+            StartCoroutine(DoDestroy());
+        }
+    }
+
+    public abstract IEnumerator DoDestroy();
+}
