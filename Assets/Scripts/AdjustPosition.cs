a4f0977839e0f7b56d619d58fd16653c83785d3d
Felipe Castilhos <felipecastilhos@gmail.com>
2019-02-21 00:15:12 -0300

    Create bat and a simple follow logic

---
+++ Assets/Scripts/AdjustPosition.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustPosition : MonoBehaviour {
+
+    public bool mustAdjustPosition;
+    [SerializeField]
+    public SpriteRenderer spriteRenderer;
+
+
+
+    // Use this for initialization
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sortingLayerName != null) {
+            spriteRenderer.sortingLayerName = "adjustP";
+            updateLayer();
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(mustAdjustPosition) {
+            if(spriteRenderer != null) {
+                updateLayer();
+            }
+        }
+    }
+
+    private void updateLayer() {
+        spriteRenderer.sortingOrder = Mathf.RoundToInt(-transform.position.y) * 100;
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float vel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chase();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(isHero(other)) {
+            print("I see you hero");
+            target = other.transform;
+        }
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (isHero(collision)) {
+            print("Ill find yout");
+            target = null;
+        }
+    }
+
+
+    private bool isHero(Collider2D collider) {
+        return collider.CompareTag("hero");
+    }
+
+    private void chase() {
+        if(target != null) {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, vel * Time.deltaTime);
+        }
+    }
+}
